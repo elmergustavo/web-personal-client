de2e68c4022e9e3c57a4bcd6acad1b5a38a9010d
elmergustavo <elmergustavo79@gmail.com>
2022-02-11 14:33:33 -0600

    gh-pages add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { DatePicker, Space } from 'antd';
+import './App.scss';
 
 function App() {
+
+
+  const test = (date, dateString) => {
+    console.log(date, dateString)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Web personal </h1>
+      <h2> Hola mundo </h2>
+      <DatePicker onChange={test} />
     </div>
   );
 }
